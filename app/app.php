d135e3b8ae774496922adb526735cce45a8efab7
Rowan Merewood <rowan@merewood.org>
2013-01-21 19:38:52 +0000

    Moved code into app directory

---
+++ app/app.php
@@ -0,0 +1,21 @@
+<?php
+
+$app = new Silex\Application();
+
+$app->get('/sort/{algorithm}', function ($algorithm) use ($app) {
+    $total = 6;
+    $elements = array();
+
+    for ($index = 0; $index < $total; $index++) {
+        $elements[$index] = mt_rand(0, 360);
+    }
+
+    $snaphots = new Sorting\IterationSnapshots();
+    $algorithm = new Sorting\QuickSort($elements);
+    $algorithm->addObserver($snaphots);
+    $algorithm->sort();
+
+    return 'Sorted';
+});
+
+return $app;
--- web/index.php
+++ web/index.php
@@ -1,23 +1,4 @@
 <?php
-
 require_once __DIR__ . '/../vendor/autoload.php';
-
-$app = new Silex\Application();
-
-$app->get('/sort/{algorithm}', function ($algorithm) use ($app) {
-    $total = 6;
-    $elements = array();
-
-    for ($index = 0; $index < $total; $index++) {
-        $elements[$index] = mt_rand(0, 360);
-    }
-
-    $snaphots = new Sorting\IterationSnapshots();
-    $algorithm = new Sorting\QuickSort($elements);
-    $algorithm->addObserver($snaphots);
-    $algorithm->sort();
-
-    return 'Sorted';
-});
-
+$app = require_once __DIR__ . '/../app/app.php';
 $app->run();
