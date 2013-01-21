ef6c13e546296fca8c3eead15284ed7c0c09c565
Rowan Merewood <rowan@merewood.org>
2013-01-21 19:30:58 +0000

    Drop spike into application

--- web/index.php
+++ web/index.php
@@ -1,10 +1,23 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
+
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
 $app->get('/sort/{algorithm}', function ($algorithm) use ($app) {
-    return 'Sorting: '.$app->escape($algorithm);
+    $total = 6;
+    $elements = array();
+
+    for ($index = 0; $index < $total; $index++) {
+        $elements[$index] = mt_rand(0, 360);
+    }
+
+    $snaphots = new Sorting\IterationSnapshots();
+    $algorithm = new Sorting\QuickSort($elements);
+    $algorithm->addObserver($snaphots);
+    $algorithm->sort();
+
+    return 'Sorted';
 });
 
 $app->run();
