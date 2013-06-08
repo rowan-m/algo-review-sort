f10eb84449cd0f0b0d842a2da415fad7587fc6a3
Rowan Merewood <rowan@merewood.org>
2013-06-08 11:57:13 +0200

    Search demos

--- app/app.php
+++ app/app.php
@@ -6,14 +6,33 @@ $app->get('/sort/{algorithm}', function (Sorting\Algorithm $algorithm) use ($app
         $app['elements.total'] = (int) $app['request']->get('total');
     }
 
-    $snapshots = $app['observer.snapshots'];
+    $snapshots = $app['observer.sort.snapshots'];
     $algorithm->addObserver($snapshots);
     $algorithm->sort($app['elements.random']);
-    return $app['twig']->render('horizontal.html.twig', array(
+    return $app['twig']->render('sort-horizontal.html.twig', array(
         'snapshots' => $snapshots,
         'embedded' => (bool) $app['request']->get('embedded'),
     ));
 })->convert('algorithm', $sortProvider);
+
+$app->get('/search/{algorithm}', function (Searching\Algorithm $algorithm) use ($app) {
+    if ($app['request']->get('total') && $app['request']->get('total') > 0) {
+        $app['elements.total'] = (int) $app['request']->get('total');
+    }
+
+    $snapshots = $app['observer.search.snapshots'];
+    $algorithm->addObserver($snapshots);
+    $elements = $app['elements.random'];
+    sort($elements);
+    $target = $elements[array_rand($elements)];
+    $algorithm->search($target, $elements);
+    return $app['twig']->render('search-horizontal.html.twig', array(
+        'elements' => $elements,
+        'target' => $target,
+        'snapshots' => $snapshots,
+        'embedded' => (bool) $app['request']->get('embedded'),
+    ));
+})->convert('algorithm', $searchProvider);
 
 $app->get('/example/stable', function () use ($app) {
     $snapshots = array(
@@ -21,7 +40,7 @@ $app->get('/example/stable', function () use ($app) {
         array('elements' => array('1:A', '1:B', '2:A', '2:B', '2:C', '3:A', '3:B', '4:B'), 'indices' => array()),
     );
 
-    return $app['twig']->render('horizontal.html.twig', array(
+    return $app['twig']->render('sort-horizontal.html.twig', array(
         'snapshots' => $snapshots,
         'embedded' => (bool) $app['request']->get('embedded'),
     ));
@@ -34,7 +53,7 @@ $app->get('/example/unstable', function () use ($app) {
         array('elements' => array('1:A', '1:B', '2:A', '2:B', '2:C', '3:A', '3:B', '4:B'), 'indices' => array()),
     );
 
-    return $app['twig']->render('horizontal.html.twig', array(
+    return $app['twig']->render('sort-horizontal.html.twig', array(
         'snapshots' => $snapshots,
         'embedded' => (bool) $app['request']->get('embedded'),
     ));
